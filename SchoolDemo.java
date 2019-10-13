55126aaea494079363634e37b6734154e9102230
smccrimon-ancoraed <56347220+smccrimon-ancoraed@users.noreply.github.com>
2019-10-13 09:46:06 -0700

    Delete schooldemo.java

--- SchoolDemo.java
+++
@@ -1,8 +0,0 @@
-public class SchoolDemo
-{
-   public static void main(String[] args)
-   {
-       School mySchool = new School("Audubon Elementary", "3500 Hoyne", 60618, 350);
-       mySchool.display();
-   }
-}  
